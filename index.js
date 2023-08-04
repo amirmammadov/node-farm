5b6e64250c303727d2ab86a312152586784ec86a
amirmammadov <amirmammado@gmail.com>
2023-08-04 23:32:30 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./module/replaceTemplate");
+const slugify = require("slugify");
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(url.parse(req.url, true));
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    let output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Something",
+    });
+    res.end("<h1>Page Not Found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Web server has been created");
+});
